2526077f041cc7014cc90a1a338ed5380a0132bb
MarceloBrito1 <76165488+MarceloBrito1@users.noreply.github.com>
2025-07-07 00:44:44 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TabelaProcessos from "./TabelaProcessos.jsx";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-2xl font-bold mb-4 text-blue-800">Painel - Legaltech Marcelo Brito</h1>
+      <TabelaProcessos />
+    </div>
+  );
+}
+
+export default App;
